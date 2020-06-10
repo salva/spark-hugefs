8e20cc6540c3ecad511727c5dfa612f0e30d32df
Salvador Fandiño <sfandino@yahoo.com>
2020-06-10 15:38:25 +0200

    backward compatibility with Scala 2.11

--- src/main/scala/com/github/salva/spark/hugefs/restriction/MaxDepth.scala
+++ src/main/scala/com/github/salva/spark/hugefs/restriction/MaxDepth.scala
@@ -3,7 +3,9 @@ package com.github.salva.spark.hugefs.restriction
 import com.github.salva.spark.hugefs.{Restriction, Verdict}
 
 class MaxDepth(val maxDepth: Long) extends(Restriction) {
-  def charInStringCount(str:String, c:Char): Long = str.chars.filter(_ == c).count
+  def charInStringCount(str:String, c:Char):Long = {
+    str.toCharArray.count(_==c)
+  }
 
   def pathDepth(path:String):Long = {
     if (path == "") 0 else 1 + charInStringCount(path, '/')
